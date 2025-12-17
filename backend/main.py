0a58b4c8264b3367c152e53d968f1e9dd2c2f6f2
Akella Satya Hanuma Saketh <63924046+sakethakella@users.noreply.github.com>
2025-12-18 00:49:02 +0530

    backend made friendly for react

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
+app = FastAPI()
+
+BUILD_DIR = os.path.abspath(os.path.join("..", "frontend", "dist"))
+ASSETS_DIR = os.path.join(BUILD_DIR, "assets")
+
+app.mount("/assets", StaticFiles(directory=ASSETS_DIR), name="assets")
+
 @app.get("/")
 def read_root():
-    return {"string": "Hello World"}
+    return FileResponse(os.path.join(BUILD_DIR, "index.html"))
+
+@app.get("/{path:path}")
+def spa(path: str):
+    return FileResponse(os.path.join(BUILD_DIR, "index.html"))
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 function App() {
   return (
     <>
+    <h2>Hello World</h2>
     </>
   )
 }
