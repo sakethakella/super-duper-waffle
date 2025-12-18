16d429d5606249185c133a12f80bf7ee3a3cb5e8
Akella Satya Hanuma Saketh <63924046+sakethakella@users.noreply.github.com>
2025-12-18 12:10:35 +0530

    navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,6 @@
 function App() {
   return (
     <>
-    <div className="min-h-screen flex items-center justify-center bg-slate-900">
-      <h1 className="text-4xl font-bold text-white">
-        Tailwind is working 🚀
-      </h1>
-    </div>
     </>
   )
 }
