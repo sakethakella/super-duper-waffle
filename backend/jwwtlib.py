7bea515ee1c4442d3a0f8f4696724d5872dff3db
Akella Satya Hanuma Saketh <63924046+sakethakella@users.noreply.github.com>
2025-12-21 17:51:48 +0530

    Update jwwtlib.py

--- backend/jwwtlib.py
+++ backend/jwwtlib.py
@@ -24,6 +24,7 @@ def verify_token(request: Request):
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except jwt.ExpiredSignatureError:
+        
         raise HTTPException(status_code=401, detail="Token expired")
     except jwt.JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
